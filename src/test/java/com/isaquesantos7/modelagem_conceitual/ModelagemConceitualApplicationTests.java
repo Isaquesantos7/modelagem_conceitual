d26782c8d475064f0938a1b3e68fa05082b3da30
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-06-30 20:14:00 -0300

    feat: projeto criado

---
+++ src/test/java/com/isaquesantos7/modelagem_conceitual/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaquesantos7.modelagem_conceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
