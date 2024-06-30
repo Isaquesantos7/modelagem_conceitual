cde5e0f48719264dba329a472fa1e66dec0e7456
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-06-30 20:48:02 -0300

    feat: persistindo dados no banco em memoria h2-console

--- src/main/java/com/isaquesantos7/modelagem_conceitual/model/Category.java
+++ src/main/java/com/isaquesantos7/modelagem_conceitual/model/Category.java
@@ -1,13 +1,19 @@
 package com.isaquesantos7.modelagem_conceitual.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
